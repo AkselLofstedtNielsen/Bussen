1c6af104b4a81ae78961fe14e992ba9ea3d9e8c6
Aksel Nielsen <akselnielsen@MacBook-Pro-som-tillhor-Aksel.local>
2022-09-20 13:12:59 +0200

    First commit

---
+++ app/src/main/java/com/example/v1bussen/Card.kt
@@ -0,0 +1,9 @@
+package com.example.v1bussen
+
+class Card (val value : Int,
+            val suit : String,
+            val faceUp : Boolean,
+            val face : Int,
+            val back : Int) {
+
+}
---
+++ app/src/main/java/com/example/v1bussen/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.v1bussen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        for (i in 1..13){
+
+        }
+    }
+
+
+}
