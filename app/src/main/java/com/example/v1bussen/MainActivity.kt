366c1204a007c5e3e03245ecf69687858529edb2
Aksel Nielsen <aksel98@outlook.com>
2022-09-21 20:04:37 +0200

    value works

--- app/src/main/java/com/example/v1bussen/MainActivity.kt
+++ app/src/main/java/com/example/v1bussen/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.v1bussen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,66 +17,69 @@ class MainActivity : AppCompatActivity() {
     lateinit var buttonLower: Button
     lateinit var buttonHigher: Button
 
-    var firstRowCard = 0 //int of value for card picked in first row
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var firstRowCardValue = 0 //int of value for card picked in first row
+        var randomCardValue = 0
+
         val card1h = Card(R.drawable.h1, 1, "h")
-        val card2h = Card(R.drawable.h2, 1, "h")
-        val card3h = Card(R.drawable.h3, 1, "h")
-        val card4h = Card(R.drawable.h4, 1, "h")
-        val card5h = Card(R.drawable.h5, 1, "h")
-        val card6h = Card(R.drawable.h6, 1, "h")
-        val card7h = Card(R.drawable.h7, 1, "h")
-        val card8h = Card(R.drawable.h8, 1, "h")
-        val card9h = Card(R.drawable.h9, 1, "h")
-        val card10h = Card(R.drawable.h11, 1, "h")
-        val card11h = Card(R.drawable.h11, 1, "h")
-        val card12h = Card(R.drawable.h12, 1, "h")
-        val card13h = Card(R.drawable.h13, 1, "h")
+        val card2h = Card(R.drawable.h2, 2, "h")
+        val card3h = Card(R.drawable.h3, 3, "h")
+        val card4h = Card(R.drawable.h4, 4, "h")
+        val card5h = Card(R.drawable.h5, 5, "h")
+        val card6h = Card(R.drawable.h6, 6, "h")
+        val card7h = Card(R.drawable.h7, 7, "h")
+        val card8h = Card(R.drawable.h8, 8, "h")
+        val card9h = Card(R.drawable.h9, 9, "h")
+        val card10h = Card(R.drawable.h11, 10, "h")
+        val card11h = Card(R.drawable.h11, 11, "h")
+        val card12h = Card(R.drawable.h12, 12, "h")
+        val card13h = Card(R.drawable.h13, 13, "h")
 
         val card1s = Card(R.drawable.s1, 1, "s")
-        val card2s = Card(R.drawable.s2, 1, "s")
-        val card3s = Card(R.drawable.s3, 1, "s")
-        val card4s = Card(R.drawable.s4, 1, "s")
-        val card5s = Card(R.drawable.s5, 1, "s")
-        val card6s = Card(R.drawable.s6, 1, "s")
-        val card7s = Card(R.drawable.s7, 1, "s")
-        val card8s = Card(R.drawable.s8, 1, "s")
-        val card9s = Card(R.drawable.s9, 1, "s")
-        val card10s = Card(R.drawable.s11, 1, "s")
-        val card11s = Card(R.drawable.s11, 1, "s")
-        val card12s = Card(R.drawable.s12, 1, "s")
-        val card13s = Card(R.drawable.s13, 1, "s")
+        val card2s = Card(R.drawable.s2, 2, "s")
+        val card3s = Card(R.drawable.s3, 3, "s")
+        val card4s = Card(R.drawable.s4, 4, "s")
+        val card5s = Card(R.drawable.s5, 5, "s")
+        val card6s = Card(R.drawable.s6, 6, "s")
+        val card7s = Card(R.drawable.s7, 7, "s")
+        val card8s = Card(R.drawable.s8, 8, "s")
+        val card9s = Card(R.drawable.s9, 9, "s")
+        val card10s = Card(R.drawable.s11, 10, "s")
+        val card11s = Card(R.drawable.s11, 11, "s")
+        val card12s = Card(R.drawable.s12, 12, "s")
+        val card13s = Card(R.drawable.s13, 13, "s")
 
         val card1r = Card(R.drawable.r1, 1, "r")
-        val card2r = Card(R.drawable.r2, 1, "r")
-        val card3r = Card(R.drawable.r3, 1, "r")
-        val card4r = Card(R.drawable.r4, 1, "r")
-        val card5r = Card(R.drawable.r5, 1, "r")
-        val card6r = Card(R.drawable.r6, 1, "r")
-        val card7r = Card(R.drawable.r7, 1, "r")
-        val card8r = Card(R.drawable.r8, 1, "r")
-        val card9r = Card(R.drawable.r9, 1, "r")
-        val card10r = Card(R.drawable.r11, 1, "r")
-        val card11r = Card(R.drawable.r11, 1, "r")
-        val card12r = Card(R.drawable.r12, 1, "r")
-        val card13r = Card(R.drawable.r13, 1, "r")
+        val card2r = Card(R.drawable.r2, 2, "r")
+        val card3r = Card(R.drawable.r3, 3, "r")
+        val card4r = Card(R.drawable.r4, 4, "r")
+        val card5r = Card(R.drawable.r5, 5, "r")
+        val card6r = Card(R.drawable.r6, 6, "r")
+        val card7r = Card(R.drawable.r7, 7, "r")
+        val card8r = Card(R.drawable.r8, 8, "r")
+        val card9r = Card(R.drawable.r9, 9, "r")
+        val card10r = Card(R.drawable.r11, 10, "r")
+        val card11r = Card(R.drawable.r11, 11, "r")
+        val card12r = Card(R.drawable.r12, 12, "r")
+        val card13r = Card(R.drawable.r13, 13, "r")
 
         val card1c = Card(R.drawable.c1, 1, "c")
-        val card2c = Card(R.drawable.c2, 1, "c")
-        val card3c = Card(R.drawable.c3, 1, "c")
-        val card4c = Card(R.drawable.c4, 1, "c")
-        val card5c = Card(R.drawable.c5, 1, "c")
-        val card6c = Card(R.drawable.c6, 1, "c")
-        val card7c = Card(R.drawable.c7, 1, "c")
-        val card8c = Card(R.drawable.c8, 1, "c")
-        val card9c = Card(R.drawable.c9, 1, "c")
-        val card10c = Card(R.drawable.c11, 1, "c")
-        val card11c = Card(R.drawable.c11, 1, "c")
-        val card12c = Card(R.drawable.c12, 1, "c")
-        val card13c = Card(R.drawable.c13, 1, "c")
+        val card2c = Card(R.drawable.c2, 2, "c")
+        val card3c = Card(R.drawable.c3, 3, "c")
+        val card4c = Card(R.drawable.c4, 4, "c")
+        val card5c = Card(R.drawable.c5, 5, "c")
+        val card6c = Card(R.drawable.c6, 6, "c")
+        val card7c = Card(R.drawable.c7, 7, "c")
+        val card8c = Card(R.drawable.c8, 8, "c")
+        val card9c = Card(R.drawable.c9, 9, "c")
+        val card10c = Card(R.drawable.c11, 10, "c")
+        val card11c = Card(R.drawable.c11, 11, "c")
+        val card12c = Card(R.drawable.c12, 12, "c")
+        val card13c = Card(R.drawable.c13, 13, "c")
 
         var deck = mutableListOf<Card>(
             card1h,
@@ -143,59 +146,59 @@ class MainActivity : AppCompatActivity() {
         buttonLower.visibility = View.INVISIBLE
         buttonHigher.visibility = View.INVISIBLE
 
-        var firstRowCard = 0 //int of value for card picked in first row
+        //int of value for card picked in first row
 
         randomCardButton.setOnClickListener {
             var random = (1..52).shuffled().first()
             randomCardButton.setImageResource(deck[random].image)
             buttonLower.visibility = View.VISIBLE
             buttonHigher.visibility = View.VISIBLE
+            randomCardValue = deck[random].value
 
         }
         bottomCard1.setOnClickListener {
-            var random = (1..52).shuffled()
-                .first()//shuffled 1..52 int so its "random" but usable again inside the {}
+            var random = (1..52).shuffled().first()//shuffled 1..52 int so its "random" but usable again inside the {}
             bottomCard1.setImageResource(deck[random].image)
             disableFirstRow()
-            firstRowCard = deck[random].value
+            firstRowCardValue = deck[random].value
+
+
+
         }
         bottomCard2.setOnClickListener {
             var random = (1..52).shuffled().first()
             bottomCard2.setImageResource(deck[random].image)
             disableFirstRow()
-            firstRowCard = deck[random].value
-        }
-        bottomCard3.setOnClickListener {
-            var random = (1..52).shuffled().first()
-            bottomCard3.setImageResource(deck[random].image)
-            disableFirstRow()
-            firstRowCard = deck[random].value
-        }
-        bottomCard4.setOnClickListener {
-            var random = (1..52).shuffled().first()
-            bottomCard4.setImageResource(deck[random].image)
-            disableFirstRow()
-            firstRowCard = deck[random].value
-        }
+            firstRowCardValue = deck[random].value
 
 
-    }
+        }
+            bottomCard3.setOnClickListener {
+                var random = (1..52).shuffled().first()
+                bottomCard3.setImageResource(deck[random].image)
+                disableFirstRow()
+                firstRowCardValue = deck[random].value
 
+
+            }
+            bottomCard4.setOnClickListener {
+                var random = (1..52).shuffled().first()
+                bottomCard4.setImageResource(deck[random].image)
+                disableFirstRow()
+                firstRowCardValue = deck[random].value
+
+
+
+
+            }
+
+        }
     fun disableFirstRow() {
         bottomCard1.isEnabled = false
         bottomCard2.isEnabled = false
         bottomCard3.isEnabled = false
         bottomCard4.isEnabled = false
     }
-
-    fun checkValueNextRow() {
-
-        if (firstRowCard != 0){//check value in rows
-
-        }
-
     }
 
 
-
-}
