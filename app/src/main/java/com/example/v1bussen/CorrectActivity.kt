cdc0bcc51015a202627b6e2f564d3450990cac30
Aksel Nielsen <aksel98@outlook.com>
2022-09-26 17:01:11 +0200

    Prize screen

--- app/src/main/java/com/example/v1bussen/CorrectActivity.kt
+++ app/src/main/java/com/example/v1bussen/CorrectActivity.kt
@@ -1,19 +1,38 @@
 package com.example.v1bussen
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import nl.dionsegijn.konfetti.KonfettiView
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 
 class CorrectActivity : AppCompatActivity() {
 
-    lateinit var textView: TextView
+    lateinit var winButton: Button
+    lateinit var konfettiView: KonfettiView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_correct)
 
-        textView = findViewById(R.id.textView)
-        textView.text ="GOOD JOB!"
+        konfettiView = findViewById(R.id.viewKonfetti)
+        winButton = findViewById(R.id.winButton)
+        winButton.text = "Click for prize!"
+
+        winButton.setOnClickListener {
+            konfettiView.build()
+                .addColors(Color.YELLOW,Color.GREEN,Color.MAGENTA)
+                .setDirection(0.0,359.0)
+                .setSpeed(1f,5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square, Shape.Circle)
+                .addSizes(Size(12))
+                .setPosition(-50f,konfettiView.width+50f,-50f,-50f)
+                .streamFor(300,3000L)
+        }
 
     }
 }
