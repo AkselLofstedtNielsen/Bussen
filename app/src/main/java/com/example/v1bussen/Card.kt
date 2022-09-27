caa119f463ee1ec92ecf06547ea5ddb874e49dbc
Aksel Nielsen <akselnielsen@MacBook-Pro-som-tillhor-Aksel.local>
2022-09-27 14:00:18 +0200

    Refreshed main code

--- app/src/main/java/com/example/v1bussen/Card.kt
+++ app/src/main/java/com/example/v1bussen/Card.kt
@@ -3,7 +3,62 @@ package com.example.v1bussen
 class Card (val value : Int,
             val suit : String,
             val faceUp : Boolean,
-            val face : Int,
             val back : Int) {
 
+
+  // val heartA = Card(1,"H", false,R.drawable.h1,R.drawable.Joker)
+  // val heart2 = Card(2,"H", false,R.drawable.h2,R.drawable.Joker)
+  // val heart3 = Card(3,"H", false,R.drawable.h3,R.drawable.Joker)
+  // val heart4 = Card(4,"H", false,R.drawable.h4,R.drawable.Joker)
+  // val heart5 = Card(5,"H", false,R.drawable.h5,R.drawable.Joker)
+  // val heart6 = Card(6,"H", false,R.drawable.h6,R.drawable.Joker)
+  // val heart7 = Card(7,"H", false,R.drawable.h7,R.drawable.Joker)
+  // val heart8 = Card(8,"H", false,R.drawable.h8,R.drawable.Joker)
+  // val heart9 = Card(9,"H", false,R.drawable.h9,R.drawable.Joker)
+  // val heart10 = Card(10,"H", false,R.drawable.h10,R.drawable.Joker)
+  // val heartJ = Card(11,"H", false,R.drawable.h11,R.drawable.Joker)
+  // val heartQ = Card(12,"H", false, R.drawable.h12,R.drawable.Joker)
+  // val heartK = Card(13,"H", false,R.drawable.h13,R.drawable.Joker)
+
+  // val spadeA = Card(1,"S", false,R.drawable.s1,R.drawable.Joker)
+  // val spade2 = Card(2,"S", false,R.drawable.s2,R.drawable.Joker)
+  // val spade3 = Card(3,"S", false,R.drawable.s3,R.drawable.Joker)
+  // val spade4 = Card(4,"S", false,R.drawable.s4,R.drawable.Joker)
+  // val spade5 = Card(5,"S", false,R.drawable.s5,R.drawable.Joker)
+  // val spade6 = Card(6,"S", false,R.drawable.s6,R.drawable.Joker)
+  // val spade7 = Card(7,"S", false,R.drawable.s7,R.drawable.Joker)
+  // val spade8 = Card(8,"S", false,R.drawable.s8,R.drawable.Joker)
+  // val spade9 = Card(9,"S", false,R.drawable.s9,R.drawable.Joker)
+  // val spade10 = Card(10,"S", false,R.drawable.s10,R.drawable.Joker)
+  // val spadeJ = Card(11,"S", false,R.drawable.s11,R.drawable.Joker)
+  // val spadeQ = Card(12,"S", false, R.drawable.s12,R.drawable.Joker)
+  // val spadeK = Card(13,"S", false,R.drawable.s13,R.drawable.Joker)
+
+  // val cloveA = Card(1,"C", false,R.drawable.c1,R.drawable.Joker)
+  // val clove2 = Card(2,"C", false,R.drawable.c2,R.drawable.Joker)
+  // val clove3 = Card(3,"C", false,R.drawable.c3,R.drawable.Joker)
+  // val clove4 = Card(4,"C", false,R.drawable.c4,R.drawable.Joker)
+  // val clove5 = Card(5,"C", false,R.drawable.c5,R.drawable.Joker)
+  // val clove6 = Card(6,"C", false,R.drawable.c6,R.drawable.Joker)
+  // val clove7 = Card(7,"C", false,R.drawable.c7,R.drawable.Joker)
+  // val clove8 = Card(8,"C", false,R.drawable.c8,R.drawable.Joker)
+  // val clove9 = Card(9,"C", false,R.drawable.c9,R.drawable.Joker)
+  // val clove10 = Card(10,"C", false,R.drawable.c10,R.drawable.Joker)
+  // val cloveJ = Card(11,"C", false,R.drawable.c11,R.drawable.Joker)
+  // val cloveQ = Card(12,"C", false, R.drawable.c12,R.drawable.Joker)
+  // val cloveK = Card(13,"C", false,R.drawable.c13,R.drawable.Joker)
+
+  // val diamondA = Card(1,"D", false,R.drawable.r1,R.drawable.Joker)
+  // val diamond2 = Card(2,"D", false,R.drawable.r2,R.drawable.Joker)
+  // val diamond3 = Card(3,"D", false,R.drawable.r3,R.drawable.Joker)
+  // val diamond4 = Card(4,"D", false,R.drawable.r4,R.drawable.Joker)
+  // val diamond5 = Card(5,"D", false,R.drawable.r5,R.drawable.Joker)
+  // val diamond6 = Card(6,"D", false,R.drawable.r6,R.drawable.Joker)
+  // val diamond7 = Card(7,"D", false,R.drawable.r7,R.drawable.Joker)
+  // val diamond8 = Card(8,"D", false,R.drawable.r8,R.drawable.Joker)
+  // val diamond9 = Card(9,"D", false,R.drawable.r9,R.drawable.Joker)
+  // val diamond10 = Card(10,"D", false,R.drawable.r10,R.drawable.Joker)
+  // val diamondJ = Card(11,"D", false,R.drawable.r11,R.drawable.Joker)
+  // val diamondQ = Card(12,"D", false, R.drawable.r12,R.drawable.Joker)
+  // val diamondK = Card(13,"D", false,R.drawable.r13,R.drawable.Joker)
 }
--- app/src/main/java/com/example/v1bussen/MainActivity.kt
+++ app/src/main/java/com/example/v1bussen/MainActivity.kt
@@ -2,16 +2,37 @@ package com.example.v1bussen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        for (i in 1..13){
+        var deck = mutableListOf<Card>()
 
+        for (hearts in 1..13) {
+            var newCard = Card(hearts, "H", false, R.drawable.Joker)
+            deck.add(newCard)
+        }
+        for (spades in 1..13) {
+            var newCard = Card(spades, "S", false, R.drawable.Joker)
+            deck.add(newCard)
+        }
+        for (clubs in 1..13) {
+            var newCard = Card(clubs, "C", false, R.drawable.Joker)
+            deck.add(newCard)
+        }
+        for (diamonds in 1..13) {
+            var newCard = Card(diamonds, "D", false, R.drawable.Joker)
+            deck.add(newCard)
+        }
+
+        for (cards in deck){
+              Log.d("!!!","HEjsan")
         }
     }
-
-
 }
+
+
